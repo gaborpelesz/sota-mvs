27ad9e6e79e5d272df4bfecda615c297cafd70f3
gaborpelesz <gabor.pelesz@aconic.ai>
2025-04-26 19:19:20 +0200

    simple script that downloads dataset, runs ACMH, and evaluates

--- eval.py
+++ eval.py
@@ -1,5 +1,14 @@
 import subprocess
 import argparse
+import sys
+import os
+
+try:
+    sys.path.append(os.path.join(os.path.dirname(__file__), "eth3d"))
+    from eth3d import prepare_datasets, ETH3D_DATASETS_TRAINING, ETH3D_DATASETS_TEST
+except ImportError:
+    print("Error: eth3d module not found. Please ensure it is installed correctly.")
+    sys.exit(1)
 
 parser = argparse.ArgumentParser()
 
@@ -7,29 +16,44 @@ parser.add_argument(
     "--datasets",
     nargs="+",
     default=[],
-    type=str,
+    choices=ETH3D_DATASETS_TRAINING + ETH3D_DATASETS_TEST,
     help="List of datasets to evaluate e.g. `--datasets courtyard`",
 )
-parser.add_argument("--ground_truth_mlp_path", type=str, required=True)
-parser.add_argument("--reconstruction_ply_path", type=str, required=True)
-parser.add_argument("--ground_truth_mlp_path", type=str, required=True)
+parser.add_argument("--width", type=int, default=None)
 
 args = parser.parse_args()
 
 # 1. download datasets
-# 2. run matrix
-# 3. run evaluation
+prepare_datasets(args.datasets, "evaluation", args.width)
 
-
-
-subprocess.call(
-    [
-        "eth3d/multi-view-evaluation/build/ETH3DMultiViewEvaluation",
-        "--reconstruction_ply_path",
-        args.reconstruction_ply_path,
-        "--ground_truth_mlp_path",
-        args.ground_truth_mlp_path,
-        "--tolerances",
-        "0.01,0.02,0.05,0.1,0.2,0.5",
-    ]
-)
+for dataset in args.datasets:
+    # 1.1 convert colmap to mvsnet format
+    subprocess.check_call(
+        [
+            sys.executable,
+            "colmap2mvsnet_acm_perf.py",
+            "--dense_folder",
+            f"evaluation/{dataset}/{dataset}_dslr_undistorted/{dataset}",
+            "--save_folder",
+            f"evaluation_mvsnet_{dataset}",
+        ]
+    )
+    # 2. will run mvs matrix, currently only ACMH
+    subprocess.check_call(
+        [
+            "ACMH/build/ACMH",
+            f"evaluation_mvsnet_{dataset}",
+        ]
+    )
+    # 3. run evaluation
+    subprocess.check_call(
+        [
+            "eth3d/multi-view-evaluation/build/ETH3DMultiViewEvaluation",
+            "--reconstruction_ply_path",
+            f"evaluation_mvsnet_{dataset}/ACMH/ACMH_model.ply",
+            "--ground_truth_mlp_path",
+            f"evaluation/{dataset}/{dataset}_dslr_scan_eval/{dataset}/dslr_scan_eval/scan_alignment.mlp",
+            "--tolerances",
+            "0.01,0.02,0.05,0.1,0.2,0.5",
+        ]
+    )
